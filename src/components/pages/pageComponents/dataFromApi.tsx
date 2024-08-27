9a51ce1d8d0477a9427c15ef6e5af42883fb1044
isaac <mwesigwai433@gmail.com>
2024-08-27 10:42:44 +0300

    created upcominghorizontalDataUpdatd file

---
+++ src/components/pages/pageComponents/dataFromApi.tsx
@@ -0,0 +1,11 @@
+import { team } from "./horizontalScroller";
+
+
+export type dataFromApi = {
+    id: string;
+    team1: team;
+    team2: team;
+    time: string;
+    date:string;
+    category: string;
+};
---
+++ src/components/pages/pageComponents/upcomingHorizontalDataUpdates.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import HorizontalScrollLabel from "./horizontalScrollLabel";
+import HorizontalScroller from "./horizontalScroller";
+import { dataFromApi } from "./dataFromApi";
+import UpcomingFootballCard from "../../cards/upcomingFootballCard";
+
+type todayHorizontalDataUpdatesProps ={
+    games:dataFromApi[];
+}
+
+const UpcomingHorizontalDataUpdates: React.FC<todayHorizontalDataUpdatesProps> = ({games}) => {
+    return (
+        <>
+            <HorizontalScrollLabel sectionTitle="upcoming" />
+            <HorizontalScroller data={games} card={UpcomingFootballCard} />
+        </>
+    )
+}
+
+export default UpcomingHorizontalDataUpdates;
