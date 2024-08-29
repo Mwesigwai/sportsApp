99df8fd12e72280cd5505c39b640f620271c914d
isaac <mwesigwai433@gmail.com>
2024-08-29 07:21:34 +0300

    modified the search components show sports for my locaition

--- src/components/pages/pageComponents/searchComponent.tsx
+++ src/components/pages/pageComponents/searchComponent.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 
-type searchComponentProps ={
-    setSearchItem:React.Dispatch<React.SetStateAction<string>>;
-    searchText:string;
+type searchComponentProps = {
+    setSearchItem: React.Dispatch<React.SetStateAction<string>>;
+    searchText: string;
 }
 
-const SearchComponent:React.FC<searchComponentProps> = ({setSearchItem, searchText}) => {
+const SearchComponent: React.FC<searchComponentProps> = ({ setSearchItem, searchText }) => {
     return (
         <div className="searchComponent">
             <form>
                 <input
                     type="text"
                     className="searchInput"
-                    placeholder="search anything sporty..." 
+                    placeholder="search anything sporty..."
                     onChange={(e => setSearchItem(e.target.value))}
                     value={searchText}
                 />
 
                 <div id="checkLabel">
                     <div className="displayOptionGroup">
-                        <input
-                            type="checkbox"
-                            name="showNearbySports"
-                            id="showNearbySports" />
-                        <span>show for my location</span>
+                        <label style={{color:'#bababa', fontFamily:"Sans serif", fontSize:11}}>
+                            <input
+                                type="checkbox"
+                                name="showNearbySports"
+                                id="showNearbySports"
+                            />
+                            {}
+                            show for my location
+                        </label>
                     </div>
                 </div>
             </form>
